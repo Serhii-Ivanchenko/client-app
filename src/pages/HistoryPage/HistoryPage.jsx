326b9cba9900a21bdfc3a947488cbed5d0e5057f
Serhii-Ivanchenko <serhiiivanchenkotan@gmail.com>
2025-07-09 22:40:02 +0300

    create Footer

--- src/pages/HistoryPage/HistoryPage.jsx
+++
@@ -1,5 +0,0 @@
-import css from './HistoryPage.module.css';
-
-export default function HistoryPage(params) {
-    return <div>HistoryPage</div>;
-};
---
+++ src/pages/RecommendationsPage/RecommendationsPage.jsx
@@ -0,0 +1,5 @@
+import css from './RecommendationsPage.module.css';
+
+export default function RecommendationsPage() {
+  return <div>RecommendationsPage</div>;
+};
