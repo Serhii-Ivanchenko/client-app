0ed11a494dd0521b0cbb514ea922b23785f71803
JuliaPriadka <150530521+JuliaPriadka@users.noreply.github.com>
2025-07-10 17:01:55 +0300

    Created Login Page

--- src/validationSchemas/LoginSchema.js
+++ src/validationSchemas/LoginSchema.js
@@ -1,6 +1,9 @@
 import * as Yup from 'yup';
 
 export const LoginSchema = Yup.object().shape({
-  email: Yup.string().required('Поле Логін повинно бути заповнене'),
-  password: Yup.string().required('Поле Пароль повинно бути заповнене'),
+  phone_number: Yup.string()
+    .matches(/^\d+$/, 'Можна вводити лише цифри')
+    .matches(/^380\d{9}$/, 'Формат телефону 380123456789')
+    .required('Це поле повинно бути заповнене'),
+  password: Yup.string().required('Це поле повинно бути заповнене'),
 });
